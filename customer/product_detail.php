8cff6180b2dab31a8afc12b696a8e8a65b3638d0
Miram-2003 <miriam.appiah>
2024-12-04 18:59:28 +0000

    add

--- customer/product_detail.php
+++ customer/product_detail.php
@@ -39,7 +39,7 @@ $cat_sub = getSubCat($product['sub_cat_id']);?>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/navbr.css">
     <link rel="stylesheet" href="../css/product_detail.css">
-
+    <link rel="stylesheet" href="../css/side.css">
 </head>
 
 <body>
